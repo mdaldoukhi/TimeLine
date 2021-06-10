14b2dc5c4e9da63c963ab120e2e159288523132b
Meshari Aldoukhi <mashry151@hotmail.com>
2021-06-10 15:09:30 +0300

    Edited TimeLine

--- src/style.js
+++ src/style.js
@@ -24,17 +24,54 @@ export const Description = styled.p`
 /* End Header Section */
 
 /* Start TimeLine Section */
-export const TimeLineBg = styled.div `
+export const TimeLineBg = styled.div`
     background-color: #212b3e;
     padding: 10px;
     div:nth-child(odd) {
         flex-direction: row-reverse;
+        span {
+            left: 50px
+        }
+        span:after {
+        position: absolute;
+        content: '';
+        width: 50px;
+        height: 8px;
+        top: 40%;
+        left: -50px;
+        }
     }
+    div:first-child{
+        span:before {
+            position: absolute;
+            content: '';
+            width: 25px;
+            height: 25px;
+            border-radius: 50%;
+            bottom: 115px;
+            left: -59.7px;
+            background-color: #69aba3;
+            }
+    }
+        
     @media (max-width: 660px) {
         div:nth-child(odd) {
         flex-direction: row;
     }
+        div:nth-child(even){
+            span {
+                left: 50px
+            }
+            span:after {
+            position: absolute;
+            content: '';
+            width: 50px;
+            height: 8px;
+            top: 40%;
+            left: -50px;
+            }
         }
+    }
 `;
 export const MainList = styled.div`
     display: flex;
@@ -42,7 +79,7 @@ export const MainList = styled.div`
     margin: 0 auto;
     position: relative;
 `;
-export const Contents = styled.div `
+export const Contents = styled.div`
     text-align: center;
     padding: 15px;
     font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
@@ -75,18 +112,32 @@ export const Line = styled.div`
     background-color: #68aba3;
     span {
         content: '';
-        height: 50px;
-        width: 50px;
-        line-height: 50px;
+        height: 75px;
+        width: 75px;
+        line-height: 75px;
         text-align: center;
         background-color: #fff;
         position: absolute;
-        border-radius: 50%;
-        right: -22px;
+        border-radius: 10px;
+        left: -120px;
         top: 30%;
         font-weight: bold;
-        font-family: sans-serif;
-        font-size: 18px;
+        font-family: Tahoma;
+        font-size: 22px;
+        color: #212b3e;
+    }
+    span:after {
+        position: absolute;
+        content: '';
+        width: 50px;
+        height: 8px;
+        background-color: #68aba3;
+        top: 40%;
+        left: 75px;
+    }
+    span:nth-child(odd){
+        right: 50%;
+        display: block;
 
     }
 `;
